9869a0d0bc4f0c0860c468ac1f0e3c22cda2f56e
izeek182 <izeek182@gmail.com>
2022-09-27 18:25:13 -0600

    template complete

--- scripts/Tables.js
+++ scripts/Tables.js
@@ -1,21 +1,49 @@
-function mainTableBuilder() {
-    var myTableDiv = document.getElementById("myDynamicTable");
+var superTable = {
+    windowWidth : 100,
+    validRange :{low:0,high:0},
+    div :   document.getElementById("myDynamicTable"),
+    members: []
+};
 
-    var table = document.createElement('TABLE');
+function Initialize(){
+    StreamerTable.forEach(streamer => {
+        let member = buildStreamerBlockHTML(streamer);
+        superTable.members.push(member);
+    });
+}
+
+function GenSuperTable(){
+    let table = document.createElement('TABLE');
     table.className = "superTable";
 
-    var tableBody = document.createElement('TBODY');
+    let tableBody = document.createElement('TBODY');
     table.appendChild(tableBody);
-    var tr = document.createElement('TR');
+
+    let tr = document.createElement('TR');
     tableBody.appendChild(tr);
-    StreamerTable.forEach(streamer => {
-        var td = document.createElement('TD');
-        td.appendChild(buildStreamerBlock(streamer));
+    console.log("building table with "+superTable.members.length+" members in it.");
+    //TODO-> Use the window size and an Element size to autoformat the page.
+    //Determine how many colloms you can have, and then find the range of valid widths and set it.
+    superTable.members.forEach(streamer => {
+        let td = document.createElement('TD');
+        td.appendChild(streamer);
         tr.appendChild(td);
     });
-    myTableDiv.appendChild(table);
+    return table;
 }
-/*
+
+
+function rebuildSuperTable() {
+    if(superTable.div.hasChildNodes()){
+        superTable.div.childNodes.forEach(child=>{
+            child.remove();
+        });
+    }
+    superTable.div.appendChild(GenSuperTable());
+}
+
+
+/* ------Start Template-----
 <table class="tables">
     <tr>
         <td><img src=$(Profile) class="profile" alt=$(Name) loading="lazy"></td>
@@ -28,41 +56,61 @@ function mainTableBuilder() {
         </td>
     </tr>
 </table>
-*/
-function buildStreamerBlock(/*String*/ Streamer) {
-    var tableBody = document.createElement('table');
-    tableBody.className = "tables"
+    ------End Template-----*/ 
+    //----------------Built by writing the actual HTML to make the layout.
+    function buildStreamerBlockHTML(/*String*/ Streamer) {
+        let tableBody = document.createElement('table');
+        tableBody.className = "tables"
+    
+        let topRow = document.createElement('tr');
+        {
+            let cell = document.createElement('td');
+            let profilePic = document.createElement('img');
+            profilePic.src = Streamer.profile;
+            profilePic.className = "profile";
+            profilePic.alt = Streamer.name;
+            cell.appendChild(profilePic);
+            topRow.appendChild(cell);
+        }
+    
+        {
+            let cell = document.createElement('td');
+            let name = document.createElement('p');
+            name.innerHTML = Streamer.name;
+            name.class     ="centerText";
+            cell.appendChild(name);
+            let twitchLink = document.createElement('a');
+            twitchLink.href = Streamer.twitchLink;
+            let twitchPic = document.createElement('img');
+            twitchPic.className = "twitch";
+            twitchLink.appendChild(twitchPic);
+            cell.appendChild(twitchLink);
+            topRow.appendChild(cell);
+        }
 
-    var topRow = document.createElement('tr');
-    {
-        var cell = document.createElement('td');
-        var profilePic = document.createElement('img');
-        profilePic.src = Streamer.profile;
-        profilePic.className = "profile";
-        profilePic.alt = Streamer.name;
-        cell.appendChild(profilePic);
-        topRow.appendChild(cell);
+        tableBody.appendChild(topRow)
+        let bottomRow = document.createElement('tr');
+    
+        return tableBody;
     }
 
-    {
-        var cell = document.createElement('td');
-        var name = document.createElement('p');
-        name.innerHTML = Streamer.name;
-        name.class     ="centerText";
-        cell.appendChild(name);
-        var twitchLink = document.createElement('a');
-        twitchLink.href = Streamer.twitchLink;
-        var twitchPic = document.createElement('img');
-        twitchPic.className = "twitch";
-        twitchLink.appendChild(twitchPic);
-        cell.appendChild(twitchLink);
-        topRow.appendChild(cell);
+    function WindowResize(){
+        let pageWidth = window.innerWidth;
+        console.log("The window has been resized so Width:"+ pageWidth);
+
+        if(!SuperTableValid()){
+            console.log("table is no longer valid");
+            rebuildSuperTable();
+        }else{
+            console.log("table is still valid");
+        }
     }
 
-    tableBody.appendChild(topRow)
-    var bottomRow = document.createElement('tr');
+    function SuperTableValid(){
+        return (superTable.windowWidth < superTable.validRange.high && superTable.windowWidth > superTable.validRange.low );
+    }
 
-    return tableBody;
-}
+Initialize();
+rebuildSuperTable();
 console.log(StreamerTable);
-mainTableBuilder();
+window.onresize = WindowResize;
